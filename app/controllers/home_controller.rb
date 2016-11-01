796d714fe3ccb5ddc6319a2625bbc3460e2e88e9
Ryan <ryan.e.levi@gmail.com>
2016-10-31 20:50:28 -0700

    form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,17 @@
 class HomeController < ApplicationController
   def index
+
+  end
+
+  def app
     test = "https://api.nutritionix.com/v1_1/search/taco-bell?results=0:20&fields=item_name,nf_calories&appId=#{ENV["N_ID"]}&appKey=#{ENV["N_KEY"]}"
     response = HTTParty.get(test)
     parsed_responsed = response.parsed_response
 
     @out = []
     parsed_responsed["hits"].each do |x|
-      @out.push x["fields"]["nf_calories"] if params["calories"].to_i <= x["fields"]["nf_calories"]
+      @out.push x["fields"]["item_name"] if params["calories"].to_i >= x["fields"]["nf_calories"]
     end
-    
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get 'app' => 'home#app'
 
   root 'home#index'
 
